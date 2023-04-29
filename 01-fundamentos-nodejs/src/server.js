8c4451661895676aea202e551e8703f6f0c1dca2
Eduardo Maia <edumaia89@outlook.com>
2023-04-29 10:24:05 -0300

    HTTP Status Code

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -4,6 +4,7 @@ const users = [];
 
 const server = http.createServer((req, res) => {
   const { method, url } = req;
+
   if (method === "GET" && url === "/users") {
     return res
       .setHeader("Content-Type", "application/json")
@@ -13,14 +14,14 @@ const server = http.createServer((req, res) => {
   if (method === "POST" && url === "/users") {
     users.push({
       id: 1,
-      name: "Fulano",
-      email: "fulano@gmai.com",
+      name: "John Doe",
+      email: "johndoe@example.com",
     });
 
-    return res.end("Criação de usuários");
+    return res.writeHead(201).end();
   }
 
-  return res.end("Hello World");
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
